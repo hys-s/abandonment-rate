1e35b5c14b688b33274bab8c30039b07d5583a39
hys-s <gu.my.10m.f.st+subpc@gmail.com>
2025-02-11 04:56:16 +0900

    カスタムイベント初期コミット

--- src/pages/First.tsx
+++ src/pages/First.tsx
@@ -1,9 +1,16 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 
 export const First: FC = () => {
+  useEffect(() => {
+    gtag('event', 'rendering_first')
+  },[])
+  const handleCickButtonFirst = () => {
+    gtag('event', 'click_button_first')
+  }
   return(
     <>
       <p>First</p>
+      <button onClick={handleCickButtonFirst}>ボタン1</button>
     </>
   )
 }
