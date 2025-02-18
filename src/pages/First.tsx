6dcc3d33a2d9fcbf180ab73656f34f3ddcde9b80
hys-s <gu.my.10m.f.st+subpc@gmail.com>
2025-02-18 09:28:46 +0900

    カスタムパラメータ追加

--- src/pages/First.tsx
+++ src/pages/First.tsx
@@ -13,6 +13,30 @@ export const First: FC = () => {
     gtag('event', 'click_button_first')
     navigate('/second')
   }
+  const handleCickButtonFirst_e = () => {
+    gtag('event', 'click_button_first', {
+      'user_group': 'east'
+    })
+    navigate('/second')
+  }
+  const handleCickButtonFirst_w = () => {
+    gtag('event', 'click_button_first', {
+      'user_group': 'west'
+    })
+    navigate('/second')
+  }
+  const handleCickButtonFirst_n = () => {
+    gtag('event', 'click_button_first', {
+      'user_group': 'north'
+    })
+    navigate('/second')
+  }
+  const handleCickButtonFirst_s = () => {
+    gtag('event', 'click_button_first', {
+      'user_group': 'south'
+    })
+    navigate('/second')
+  }
 
   const handleClickButtonCountUp = () => {
     setCount((prev) => prev + 1)
@@ -22,6 +46,10 @@ export const First: FC = () => {
     <>
       <p>First</p>
       <button onClick={handleCickButtonFirst}>ボタン1</button>
+      <button onClick={handleCickButtonFirst_e}>ボタン_e</button>
+      <button onClick={handleCickButtonFirst_w}>ボタン_w</button>
+      <button onClick={handleCickButtonFirst_n}>ボタン_n</button>
+      <button onClick={handleCickButtonFirst_s}>ボタン_s</button>
 
       <span>{count}</span>
       <br />
